7898415a1314a59057910eaa25faca1bafc640c1
Marima Andrew Mambondiumwe <andrewmmambo@gmail.com>
2017-07-20 17:17:46 -0400

    Add files via upload

---
+++ Mambondiumwe-A06/Mambondiumwem-A06.cpp
@@ -0,0 +1,14 @@
+/* CSC236 Introduction to Programming with C++
+Name: Marima Andrew Mambondiumwe
+Acknowledgements: Dr Jan Pearce
+Assignment A06 : First C++ program
+Description: To familiarize myself with C++ and code blocks IDE.*/
+
+#include <iostream>         // this statement includes the input/output library
+using namespace std;        // this is for the standard  name space
+
+int main()              // all C++ programs will begin with a main function
+{
+h    cout << "Hello world!,My name is Marima" << endl;j      //cout stands for "console output"
+    return 0;                                               //this returns control to the calling system and indicates no errors occured.
+}jmbb
---
+++ Mambondiumwe-A06/test.c
@@ -0,0 +1,11 @@
+#include <iostream> //needed for cout
+using namespace std;
+
+int main()
+{
+
+  cout << "Hello World!" << endl; //Note that endl is an end of line.
+  cout << "Hello" << " World" << "!" << endl;
+
+  return 0;
+}
